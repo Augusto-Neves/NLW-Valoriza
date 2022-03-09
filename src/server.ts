ddfbbd77b054c7e1f61c8a5e19e3ced7b4d033fd
Augusto Neves <augustoneves@frwk.com.br>
2022-03-09 09:47:34 -0300

    Adding rules to create Tags on database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
-import express from 'express';
 import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import './database';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3030, () => {
     console.log('🚀 Server is running on port 3030 🚀');
